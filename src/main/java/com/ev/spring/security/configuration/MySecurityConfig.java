70943ece11802a37cd2d660bd7b972bc15eb3089
EvgeniyOVorobev <vorobiev71@yandex.ru>
2023-03-10 21:11:46 +0300

    + BD

--- src/main/java/com/ev/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/ev/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ev.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,15 +8,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication().withUser(userBuilder.username("EV").password("EV").roles("Employye"));
-        auth.inMemoryAuthentication().withUser(userBuilder.username("Elena").password("Elena").roles("HR"));
-        auth.inMemoryAuthentication().withUser(userBuilder.username("Ivan").password("Ivan").roles("HR","Manager"));
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication().withUser(userBuilder.username("EV").password("EV").roles("Employye"));
+//        auth.inMemoryAuthentication().withUser(userBuilder.username("Elena").password("Elena").roles("HR"));
+//        auth.inMemoryAuthentication().withUser(userBuilder.username("Ivan").password("Ivan").roles("HR","Manager"));
+          auth.jdbcAuthentication().dataSource(dataSource);
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
